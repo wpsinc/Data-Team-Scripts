704b927e526779d52b2b974973546af6243e1959
mpart1990 <150452757+mpart1990@users.noreply.github.com>
2024-04-10 11:27:46 -0600

    Fixed strip error

--- project/transform_scripts/fitment/OnlineFitment.py
+++ project/transform_scripts/fitment/OnlineFitment.py
@@ -3,8 +3,7 @@ import json
 
 # Specify the file path
 file_path = r"C:\Users\megan.partridge\OneDrive - Arrowhead EP\Data Tech\Fitment Audit\Fitment Templates\Offroad\Fitment Json\Online Fitment.json"
-output_file_path = r"C:\Users\megan.partridge\OneDrive - Arrowhead EP\Data Tech\Fitment Audit\Fitment Templates\Offroad\onlinefitment.csv"
-Fitment_path = r"C:\Users\megan.partridge\OneDrive - Arrowhead EP\Data Tech\Fitment Audit\Fitment Templates\Online Fitment.json"
+
 # Read the JSON data
 with open(file_path, "r") as json_file:
     data = json.load(json_file)
@@ -12,11 +11,21 @@ with open(file_path, "r") as json_file:
 # Extract relevant information from the "vehicles" section (if available)
 vehicle_data = data["data"]
 rows = []
-
+# Iterate through vehicle data entries
 for entry in vehicle_data:
     if "vehicles" in entry and "data" in entry["vehicles"] and entry["vehicles"]["data"]:
         # Assuming you want all vehicle IDs next to the corresponding SKU
-        vehicle_ids = [str(v["id"]) for v in entry["vehicles"]["data"] if v["id"] and v["id"].strip() != "-"]  # Exclude empty IDs and hyphens
+        vehicle_ids = []
+        for v in entry["vehicles"]["data"]:
+            try:
+                # Attempt to strip if v["id"] is a string
+                vehicle_id = str(v["id"]).strip()
+                if vehicle_id != "-":
+                    vehicle_ids.append(vehicle_id)
+            except AttributeError:
+                # If v["id"] is not a string, handle it accordingly
+                pass
+
         sku = str(entry["sku"])  # Convert to string
         for vehicle_id in vehicle_ids:
             rows.append((sku, vehicle_id))
@@ -27,6 +36,8 @@ for entry in vehicle_data:
 # Create a DataFrame
 df = pd.DataFrame(rows, columns=["Skus", "vehicle_ids"])
 df["concat"] = df["vehicle_ids"] + "&" + df["Skus"]
+
+output_file_path = r"C:\Users\megan.partridge\OneDrive - Arrowhead EP\Data Tech\Fitment Audit\Fitment Templates\Offroad\onlinefitment.csv"
 
 # Save the DataFrame to a CSV file
 df.to_csv(output_file_path, index=False)
