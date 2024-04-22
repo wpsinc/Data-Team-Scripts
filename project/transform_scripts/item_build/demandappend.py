62c81aec44e637efe0ea1def4032df2b57ab0550
mpart1990 <150452757+mpart1990@users.noreply.github.com>
2024-04-22 14:18:52 -0600

    Fixed Bugs with file being replaced.

--- project/transform_scripts/item_build/demandappend.py
+++ project/transform_scripts/item_build/demandappend.py
@@ -18,15 +18,14 @@ def get_input_file_path():
     file_path = filedialog.askopenfilename(defaultextension=".xlsm", filetypes=[("Excel files", "*.xlsm")])
     return file_path
 
+Demand = pd.read_excel(demand_file_path, sheet_name="Purchasing")
 # Read data from the Purchasing tab
 df_existing = pd.read_excel(get_input_file_path(), sheet_name="Purchasing")
 
-# Create new records (example data) from the Purchasing tab
-# You can modify this part to include actual data
 new_records = df_existing.copy()  # Example: Copy existing data
 
 # Append new records to existing data
-df_combined = pd.concat([df_existing, new_records], ignore_index=True)
+df_combined = pd.concat([Demand, new_records], ignore_index=True)
 
 # Save the combined data to the specified output file
 df_combined.to_excel(demand_file_path, sheet_name="Purchasing", index=False)
